7b9f157f1bd5c8b814a262da0c66e5b700eb9878
Vadim Tagunov <vadimtagunov@gmail.com>
2017-04-05 20:51:05 +0300

    Working Char Switcher (from string to array and then only, without punctoation marks)

---
+++ src/CharSwitcher/CharSwitcher.java
@@ -0,0 +1,42 @@
+package CharSwitcher;
+
+import java.util.*;
+
+public class CharSwitcher {
+
+	private String text = null;
+	private String[] words = null;
+	private int order;
+	private char symbol;
+	
+	public CharSwitcher() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your String: ");
+		text = sc.nextLine();
+		System.out.println("Now enter your order: ");
+		order = sc.nextInt();
+		System.out.println("Finally, enter your symbol: ");
+		symbol = sc.next().charAt(0);
+		System.out.println("Great Job!");
+		sc.close();
+	}
+	
+	public CharSwitcher(String text, String[] words, int order, char symbol) {
+		this.text = text;
+		this.words = words;
+		this.order = order;
+		this.symbol = symbol;
+	}
+	
+	public void Switch() {
+		words = text.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].length() >= (order)){
+				System.out.println(words[i].substring(0, order) + symbol + words[i].substring(order + 1));
+			}
+			else {
+				System.out.println(words[i]);
+			}
+		}
+	}
+}
---
+++ src/CharSwitcher/CharSwitcherMain.java
@@ -0,0 +1,10 @@
+package CharSwitcher;
+
+public class CharSwitcherMain {
+
+	public static void main(String[] args) {
+		CharSwitcher sw = new CharSwitcher();
+		sw.Switch();
+	}
+
+}
