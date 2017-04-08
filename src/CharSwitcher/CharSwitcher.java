2b3bea1b886f047d6febe40ef45ac1c1212ddfee
Vadim Tagunov <vadimtagunov@gmail.com>
2017-04-08 13:24:56 +0300

    working CharSwitcher with punctuation marks

--- src/CharSwitcher/CharSwitcher.java
+++ src/CharSwitcher/CharSwitcher.java
@@ -1,12 +1,13 @@
 package CharSwitcher;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class CharSwitcher {
 
 	private String text = null;
 	private String[] words = null;
-	private int order;
+	private int pos;
 	private char symbol;
 	
 	public CharSwitcher() {
@@ -14,28 +15,28 @@ public class CharSwitcher {
 		System.out.println("Enter your String: ");
 		text = sc.nextLine();
 		System.out.println("Now enter your order: ");
-		order = sc.nextInt();
+		pos = sc.nextInt();
 		System.out.println("Finally, enter your symbol: ");
 		symbol = sc.next().charAt(0);
 		System.out.println("Great Job!");
 		sc.close();
 	}
 	
-	public CharSwitcher(String text, String[] words, int order, char symbol) {
+	public CharSwitcher(String text, String[] words, int pos, char symbol) {
 		this.text = text;
 		this.words = words;
-		this.order = order;
+		this.pos = pos;
 		this.symbol = symbol;
 	}
 	
 	public void Switch() {
-		words = text.split(" ");
+		words = text.split("[ ,.:;?!]");
 		for (int i = 0; i < words.length; i++) {
-			if (words[i].length() >= (order)){
-				words[i] = words[i].substring(0, order) + symbol + words[i].substring(order + 1);
+			if (words[i].length() > (pos)){
+				words[i] = words[i].substring(0, pos) + symbol + words[i].substring(pos + 1);
 			}
 			else {
-				
+				words[i] = words[i];
 			}
 		}
 		for (int i = 0; i < words.length; i++) {
